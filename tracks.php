e976230ce69176187f0c49189fb02d48e20fc7ab
Emanuel Azage <eazage@usc.edu>
2019-01-18 11:37:39 -0800

    Attributes for track populates for any genre. Fully Working version.

--- tracks.php
+++ tracks.php
@@ -4,28 +4,34 @@
   $sql = "
     SELECT genres.GenreId
     From genres
-    WHERE genres.Name=" . "'" . $_GET['genre'] . "'";
+    WHERE genres.Name = :name";
 
   $statement = $pdo->prepare($sql);
-  var_dump($statement);
+  $statement->bindParam(':name', $_GET['genre'], PDO::PARAM_STR, strlen($_GET['genre']));
   $statement->execute();
-  $genreId = $statement->fetchAll(PDO::FETCH_OBJ);
-  $genreId = (int)$genreId;
 
-  var_dump($genreId);
+  $genreIdObj = $statement->fetchAll(PDO::FETCH_OBJ);
+  $genreId = $genreIdObj[0]->GenreId;
 
   $sql2 = "
     SELECT
       tracks.Name,
-      tracks.Composer
-    From tracks WHERE tracks.GenreId=" . $genreId
-    . "
-    LIMIT 15";
+      tracks.UnitPrice,
+      albums.Title,
+      artists.artistId
+    From tracks
+    INNER JOIN albums
+    ON tracks.albumId = albums.albumId
+    INNER JOIN artists
+    ON albums.artistId = artists.artistId
+    WHERE tracks.GenreId = :genreId";
 
   $statement2 = $pdo->prepare($sql2);
+  $statement2->bindParam(':genreId', $genreId, PDO::PARAM_INT);
   $statement2->execute();
 
   $tracks = $statement2->fetchAll(PDO::FETCH_OBJ);
+
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -35,10 +41,41 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
 </head>
 <body>
-  <?php
-    foreach ($tracks as $track) {
-      echo $track->Name . "<br>";
-    }
-  ?>
+  <table class='table'>
+    <tr>
+      <th>Track Name</th>
+      <th>Artist Name</th>
+      <th>Album Title</th>
+      <th>Price</th>
+    </tr>
+    <?php foreach ($tracks as $track) : ?>
+      <tr>
+        <td>
+          <?php echo $track->Name ?>
+        </td>
+        <td>
+          <?php
+            // there is probably a better way of doing this. TODO Figure out better way.
+              $sql3 = "
+                SELECT artists.Name
+                From artists
+                WHERE artists.artistId = ?
+              ";
+              $statement3 = $pdo->prepare($sql3);
+              $statement3->bindParam(1, $track->ArtistId, PDO::PARAM_INT);
+              $statement3->execute();
+              $artistName = $statement3->fetchALL(PDO::FETCH_OBJ);
+              echo $artistName[0]->Name;
+            ?>
+        </td>
+        <td>
+          <?php echo $track->Title ?>
+        </td>
+        <td>
+          <?php echo $track->UnitPrice ?>
+        </td>
+      </tr>
+    <?php endforeach ?>
+  </table>
 </body>
 </html>
