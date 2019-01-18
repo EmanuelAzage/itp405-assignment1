54202b07d3b73ac575ec71371116a32ae636007a
Emanuel Azage <eazage@usc.edu>
2019-01-18 09:25:59 -0800

    Passing genre to tracks.php and able to get tracks. But it always gets tracks for Rock genre.

---
+++ tracks.php
@@ -0,0 +1,42 @@
+<?php
+  $pdo = new PDO('sqlite:chinook.db');
+
+  $sql = "
+    SELECT genres.GenreId
+    From genres
+    WHERE genres.Name=" . "'" . $_GET['genre'] . "'";
+
+  $statement = $pdo->prepare($sql);
+  var_dump($statement);
+  $statement->execute();
+  $genreId = $statement->fetchAll(PDO::FETCH_OBJ);
+  $genreId = (int)$genreId;
+
+  var_dump($genreId);
+  
+  $sql2 = "
+    SELECT
+      tracks.Name,
+      tracks.Composer
+    From tracks WHERE tracks.GenreId=" . $genreId;
+
+  $statement2 = $pdo->prepare($sql2);
+  $statement2->execute();
+
+  $tracks = $statement2->fetchAll(PDO::FETCH_OBJ);
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Tracks</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+</head>
+<body>
+  <?php
+    foreach ($tracks as $track) {
+      echo $track->Name . "<br>";
+    }
+  ?>
+</body>
+</html>
